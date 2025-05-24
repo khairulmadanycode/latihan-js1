60a0f68e087488a4dd522237bdb5f3c0216ddbad
khairulmadanycode <loginkhairul@gmail.com>
2025-05-24 14:26:49 +0700

    instalasi awal

---
+++ assets/js/myscript.js
@@ -0,0 +1,34 @@
+function ganti_teks() {
+    const masukan = document.getElementById("masukan");
+
+    const element = document.getElementById("bagian_judul");
+    element.innerText = masukan.value
+    console.log(masukan.value)
+
+}
+
+function tampilkan_alert () {
+     const masukan = document.getElementById("masukan_1");
+
+    alert(masukan.value)
+}
+
+function lihat_judul() {
+    console.log("ini lihat judul")
+    const element = document.getElementById("bagian_judul");
+    element.classList.remove('sbok')
+    element.classList.add('gitak')
+
+}
+
+function sembunyikan_judul() {
+    console.log("ini sembunyikan")
+    const bagianjudul = document.getElementById("bagian_judul");
+    bagianjudul.classList.remove('gitak')
+    bagianjudul.classList.add('sbok')
+}
+
+function lihat_alert () {
+    console.log ("lihat_alert")
+    alert(masukan_2.value)
+}
