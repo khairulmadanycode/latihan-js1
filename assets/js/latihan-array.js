108d66011f8320d6683995d92f94bad15f6d02e9
hamdani <hamdani.inf@gmail.com>
2025-05-26 13:23:20 +0700

    latihan array

---
+++ assets/js/latihan-array.js
@@ -0,0 +1,27 @@
+
+
+/**
+ * Array yang contoh
+ * */ 
+const arrayContoh=['H','A','M'];
+
+// Perulangan
+for (let index = 0; index < 3; index++) {
+    const element = arrayContoh[index];
+    console.log(element);    
+}
+
+/**
+ * Array yang meyimpan character(Huruf)
+ * */ 
+const arrayCharacter=['K','H','A','I','R','U','L'];
+
+/**
+ * Array yang meyimpan string(kalimat/kata)
+ * */ 
+const arrayString=["Hamdani","Khairl Madani","Nurul Hidayah","Nikmatul aini","Naky"];
+
+/**
+ * Array yang meyimpan number(integer/float)
+ * */ 
+const arrayNumber=[1,2,3,4,5,6,7,8,9,10.13,11.5];
