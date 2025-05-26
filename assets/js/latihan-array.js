90046e23450d421e6221d18acdd379c84554cdad
khairulmadanycode <loginkhairul@gmail.com>
2025-05-26 14:13:13 +0700

    Finsih Latihan form dan menampilkan di log dan alert

--- assets/js/latihan-array.js
+++ assets/js/latihan-array.js
@@ -11,17 +11,33 @@ for (let index = 0; index < 3; index++) {
     console.log(element);    
 }
 
+
+const arrayCharacter=['K','H','A','I','R','U','L'];
 /**
  * Array yang meyimpan character(Huruf)
  * */ 
-const arrayCharacter=['K','H','A','I','R','U','L'];
+for (let index = 0; index < 3; index++) {
+    const element = arraycharacter[index];
+    console.log(element);    
+}
+
 
 /**
  * Array yang meyimpan string(kalimat/kata)
  * */ 
 const arrayString=["Hamdani","Khairl Madani","Nurul Hidayah","Nikmatul aini","Naky"];
 
+for (let index = 0; index < 3; index++) {
+    const element = arraycharacter[index];
+    console.log(element);    
+}
+
 /**
  * Array yang meyimpan number(integer/float)
  * */ 
 const arrayNumber=[1,2,3,4,5,6,7,8,9,10.13,11.5];
+
+for (let index = 0; index < 3; index++) {
+    const element = arraycharacter[index];
+    console.log(element);    
+}
