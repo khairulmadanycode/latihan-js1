db2fe3521bc84fe2dde9c899f991013815e68c94
khairulmadanycode <loginkhairul@gmail.com>
2025-05-26 14:38:55 +0700

    Finsih Latihan form dan menampilkan di log dan alert

--- assets/js/latihan-array.js
+++ assets/js/latihan-array.js
@@ -2,42 +2,42 @@
 
 /**
  * Array yang contoh
- * */ 
-const arrayContoh=['H','A','M'];
+ * */
+const arrayContoh = ['H', 'A', 'M'];
 
 // Perulangan
 for (let index = 0; index < 3; index++) {
     const element = arrayContoh[index];
-    console.log(element);    
+    console.log(element);
 }
 
 
-const arrayCharacter=['K','H','A','I','R','U','L'];
+const arraycharacter = ['K', 'H', 'A', 'I', 'R', 'U', 'L'];
 /**
  * Array yang meyimpan character(Huruf)
- * */ 
+ * */
 for (let index = 0; index < 3; index++) {
     const element = arraycharacter[index];
-    console.log(element);    
+    console.log(element);
 }
 
 
 /**
  * Array yang meyimpan string(kalimat/kata)
- * */ 
-const arrayString=["Hamdani","Khairl Madani","Nurul Hidayah","Nikmatul aini","Naky"];
+ * */
+const arraystring = ["Hamdani", "Khairl Madani", "Nurul Hidayah", "Nikmatul aini", "Naky"];
 
 for (let index = 0; index < 3; index++) {
-    const element = arrayString[index];
-    console.log(element);    
+    const element = arraystring[index];
+    console.log(element);
 }
 
 /**
  * Array yang meyimpan number(integer/float)
- * */ 
-const arrayNumber=[1,2,3,4,5,6,7,8,9,10.13,11.5];
+ * */
+const arraynumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10.13, 11.5];
 
 for (let index = 0; index < 3; index++) {
-    const element = arrayNumber[index];
-    console.log(element);    
+    const element = arraynumber[index];
+    console.log(element);
 }
