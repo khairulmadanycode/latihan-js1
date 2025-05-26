cc7097faca63079dec56842db1ad91ea30666780
hamdani <hamdani.inf@gmail.com>
2025-05-26 18:47:56 +0700

    Tugas pemanggilan array secara manual

---
+++ assets/js/latihan-array2.js
@@ -0,0 +1,47 @@
+
+/**
+ * Array ini tidak boleh di ubah
+ * */ 
+const arrayString = [
+        "Hamdani", 
+        "Khairul Madani", 
+        "Nurul Hidayah", 
+        "Nikmatul aini", 
+        "Naky",
+        "Nuri maulida",
+        "Kaspi"
+    ];
+
+/**
+ * Contoh menampilkan hamdani dan kahirul madani
+ * */ 
+console.log("===============================");
+console.log(arrayString[0]);
+console.log(arrayString[1]);
+console.log("===============================");
+
+/**
+ * Tugasss II
+ * tampilkan nama di bawah ini seperti contoh di asas, tapi urutannya harus sama seperti di bawah:
+ * 
+ * Hamdani
+ * Naky
+ * Nuri Maulida
+ * Kaspi
+ * 
+ * Kerjakan di bawah
+ * */ 
+
+/**
+ * Tugasss I
+ * tampilkan nama di bawah ini seperti contoh di asas, tapi urutannya harus sama seperti di bawah:
+ * 
+ * Naky
+ * Nuri Maulida
+ * Nurul Hidyah
+ * Kaspi
+ * Hamdani
+ * 
+ * Kerjakan di bawah
+ * */ 
+
