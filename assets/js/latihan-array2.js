8e50d2871d65e393cf881ff723a4c67835c626cf
khairulmadanycode <loginkhairul@gmail.com>
2025-05-27 14:44:43 +0700

    Finsih Latihan form dan menampilkan di log dan alert

--- assets/js/latihan-array2.js
+++ assets/js/latihan-array2.js
@@ -1,20 +1,20 @@
 
 /**
  * Array ini tidak boleh di ubah
- * */ 
+ * */
 const arrayString = [
-        "Hamdani", 
-        "Khairul Madani", 
-        "Nurul Hidayah", 
-        "Nikmatul aini", 
-        "Naky",
-        "Nuri maulida",
-        "Kaspi"
-    ];
+    "Hamdani",
+    "Khairul Madani",
+    "Nurul Hidayah",
+    "Nikmatul aini",
+    "Naky",
+    "Nuri maulida",
+    "Kaspi"
+];
 
 /**
  * Contoh menampilkan hamdani dan kahirul madani
- * */ 
+ * */
 console.log("===============================");
 console.log(arrayString[0]);
 console.log(arrayString[1]);
@@ -30,7 +30,7 @@ console.log("===============================");
  * Kaspi
  * 
  * Kerjakan di bawah
- * */ 
+ * */
 
 /**
  * Tugasss I
@@ -43,5 +43,40 @@ console.log("===============================");
  * Hamdani
  * 
  * Kerjakan di bawah
- * */ 
+ * */
+// SOAL KE 2
+const data = [
+    "naky",
+    "nuri maulida",
+    "nurul hidayah",
+    "kaspy",
+    "hamdani",
+];
+
+console.log("===============================");
+console.log(data[0]);
+console.log(data[1]);
+console.log(data[2]);
+console.log(data[3]);
+console.log(data[4]);
+console.log("===============================");
+
+// SOAL KE 2 END
+
+// SOAL KE 1
+const project = [
+    "hamdani",
+    "naky",
+    "nuri maulida",
+    "kaspi",
+];
+
+console.log("===============================");
+console.log(data[0]);
+console.log(data[1]);
+console.log(data[2]);
+console.log(data[3]);
+console.log("===============================");
+
+// SOAL KE 1 END
 
