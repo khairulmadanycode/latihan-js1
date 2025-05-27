799d5308168691b81e3a11394e46f2c105563b76
khairulmadanycode <loginkhairul@gmail.com>
2025-05-27 15:02:11 +0700

    Finsih Latihan form dan menampilkan di log dan alert

--- assets/js/latihan-array2.js
+++ assets/js/latihan-array2.js
@@ -31,22 +31,9 @@ console.log("===============================");
  * 
  * Kerjakan di bawah
  * */
-// SOAL KE 2
-const project = [
-    "hamdani",
-    "naky",
-    "nuri maulida",
-    "kaspi",
-];
 
-console.log("===============================");
-console.log(data[0]);
-console.log(data[1]);
-console.log(data[2]);
-console.log(data[3]);
-console.log("===============================");
 
-// SOAL KE 2 END
+
 
 /**
  * Tugasss I
@@ -80,3 +67,19 @@ console.log("===============================");
 // SOAL KE 1 END
 
 
+// SOAL KE 2
+const project = [
+    "hamdani",
+    "naky",
+    "nuri maulida",
+    "kaspi",
+];
+
+console.log("===============================");
+console.log(data[0]);
+console.log(data[1]);
+console.log(data[2]);
+console.log(data[3]);
+console.log("===============================");
+
+// SOAL KE 2 END
