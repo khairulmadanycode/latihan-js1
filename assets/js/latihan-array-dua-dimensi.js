9b35ec4ccde391272379d0a4c42266c6454f8ed7
hamdani <hamdani.inf@gmail.com>
2025-05-29 22:19:58 +0700

    Tugas array 2 dimensi

---
+++ assets/js/latihan-array-dua-dimensi.js
@@ -0,0 +1,25 @@
+
+/**
+ * Array array 2 dimensi
+ * */
+const arrayString = [
+    ["Hamdani","Khairul Madani","Nurul Hidayah"] // baris 1 index ke 0
+    ["Nikmatul aini","Naky","Nuri maulida","Kaspi"] // baris ke 2 index ke 1
+];
+
+// ini untuk menampilkan Nurul hidayah dan Naky
+console.log(arrayString[0][2]);
+console.log(arrayString[1][3]);
+
+/**
+ * Log 
+ * =====================
+ * hamdani
+ * kaspi
+ * Khairul Madani
+ * Nurul Hidayah
+ * Naky
+ * ============
+ * Kerjakan di bawah ini
+ * */ 
+
